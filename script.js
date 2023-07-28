8ea4026b3f53d47bf40f44cd6925ede48e880fa2
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-28 22:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	if(text == "  "){
+		return "";
+	}
+	else {
+		let array = text.trim().split{''};
+		return array[0];
+		
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
